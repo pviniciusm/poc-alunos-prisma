48f5df49a72387c9337ed1282dbde2176b28bc0f
pviniciusm <pvinics@gmail.com>
2023-08-30 19:43:53 -0300

    feat: express initial config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("config done");
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const port = 3333;
+
+app.listen(3333, () => {
+    console.log(`API is running at port ${port}`);
+});
